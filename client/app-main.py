664a084a09709cc7214c165b64b0c826a92d83e6
nxszy <nizinski.szy@gmail.com>
2023-12-14 22:58:00 +0100

    Created basic project structure. Retrieved older version of the code.

---
+++ client/app-main.py
@@ -0,0 +1,42 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+
+from kivy.core.window import Window
+Window.size = (420, 640)
+
+Builder.load_file("app-main.kv")
+
+class Login(MDScreen):
+    pass
+
+class Register(MDScreen):
+    pass
+
+class Menu(MDScreen):
+    pass
+
+class Learning(MDScreen):
+    pass
+
+class Profile(MDScreen):
+    pass
+
+class UserSettings(MDScreen):
+    pass
+
+class Manager(MDScreenManager):
+    pass
+
+class app(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.material_style = "M3"
+        self.theme_cls.primary_palette = "DeepOrange"
+        self.theme_cls.accent_palette = "Red"
+        sm = Manager()
+    
+        return sm
+
+app().run()
