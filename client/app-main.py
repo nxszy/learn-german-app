df8694a32c11a2c890bbc2cb76c708ee10fecbd9
nxszy <nizinski.szy@gmail.com>
2023-12-18 17:01:11 +0100

    Implemented basic 2nd learning menu - allowing to choose verbSet.

--- client/app-main.py
+++ client/app-main.py
@@ -5,7 +5,10 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.button import MDRaisedButton
 from os import listdir, path
+from functools import partial
 
 Config.set('graphics', 'resizable', False)
 
@@ -22,8 +25,44 @@ class Register(MDScreen):
 class Menu(MDScreen):
     pass
 
-class Learning(MDScreen):
-    pass
+class LearningHome1(MDScreen):
+    
+    def handle_button(self, id):
+        print(id)
+        match str(id):
+            case "learn_Conj":
+                self.manager.learningChosen = "conj"
+            case "learn_Past":
+                self.manager.learningChosen = "pastforms"
+            case "learn_Rection":
+                self.manager.learningChosen = "rection"
+
+        if self.manager.learningChosen:
+            self.manager.current = "learningHome2"
+            
+class LearningHome2(MDScreen):
+
+    btnBox = None
+    
+    def on_enter(self, *args):
+
+        box = BoxLayout(orientation="vertical", spacing="5dp")
+        self.btnBox = box
+        
+        default_btn = MDRaisedButton(id="0", text="default", size_hint=(1, None), height="300", on_release=partial(self.handle_set_button))
+        box.add_widget(default_btn)
+
+        self.ids['setList'].add_widget(box)
+    
+    def handle_set_button(self, id):
+        
+        self.manager.verbsSet = [] # get request w zależności od pId z bazy - id guzika to pId w bazie
+
+        self.manager.current = self.manager.learningChosen
+
+    def on_leave(self):
+        self.ids['setList'].remove_widget(self.btnBox)
+
 
 class ConjugationHomeScreen(MDScreen):
     pass
@@ -47,7 +86,9 @@ class Manager(MDScreenManager):
     pass
 
 class LearningManager(MDScreenManager):
-    pass
+    
+    learningChosen = ""
+    verbsSet = []
 
 class ProfileManager(MDScreenManager):
     pass
