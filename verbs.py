2103258f871631b0b1ce5cff1e0d04bac62a7900
nxszy <nizinski.szy@gmail.com>
2024-03-10 12:30:30 +0100

    Finished working on webscrapper. All verbs for conjugation are processed.

---
+++ verbs.py
@@ -0,0 +1,19 @@
+import pandas as pd
+ 
+df_infinitive = pd.read_csv("verbs.csv", delimiter=",", header=None, index_col=None)
+df_forms = pd.read_csv("forms.csv", delimiter=",", header=None, index_col=None)
+
+df_final = pd.concat([df_infinitive, df_forms], axis=1)
+column_names = ["translation", "infinitive", "sg_Ip_form", "sg_IIp_form", "sg_IIIp_form", "pl_Ip_form", "pl_IIp_form", "pl_IIIp_form"]
+
+df_final.columns = column_names
+
+print(df_final.head())
+'''
+col_list = list(df.columns)
+x, y = col_list.index(0), col_list.index(1)
+col_list[y], col_list[x] = col_list[x], col_list[y]
+df = df[col_list]
+'''
+
+df_final.to_csv("verbs_final.csv", index=False, header=True)
---
+++ webscrapper.py
@@ -0,0 +1,55 @@
+import requests
+import re
+from concurrent.futures import ThreadPoolExecutor
+
+def extract_forms(verbs):
+    
+    for verb in verbs:
+
+        url = "https://pl.bab.la/koniugacja/niemiecki/" + verb
+        print(url)
+        try:
+
+            session_obj = requests.Session()
+            response = session_obj.get(url, headers={"User-Agent": "Mozilla/5.0"})
+
+            forms = []
+            for form in ("ich", "du", "er/sie/es", "wir", "ihr", "sie/Sie"):
+                pattern = '<div class="conj-person">{}</div>.*?<div class="conj-result">(.*?)</div>'.format(form)
+                try:
+                    match = re.search(pattern, response.text, re.DOTALL | re.IGNORECASE).group(1)
+                except AttributeError as e:
+                    print(f"Error whie procesing {verb} : {e}")
+
+                forms.append(match)
+
+            all_forms.append(forms)
+
+        except requests.RequestException as e:
+            print(f"Error fetching data for verb {verb}: {e}")
+
+
+#with open("kod_html.txt", "w", encoding="utf-8") as f:
+#    f.write(response.text)
+
+all_forms = []
+
+with open("verbs.csv", "r+", encoding="utf-8") as file:
+    text = file.read()
+    verbs = [line.split(',')[1] for line in text.split('\n')]
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(extract_forms, [verbs[i:i+20] for i in range(0, len(verbs), 20)])
+
+print(all_forms)
+
+with open("forms.csv", "w+", encoding="utf-8") as f:
+    
+    for package in all_forms:
+        f.write(",".join(package) + "\n")
+
+
+
+
+
+ 
