d51d035d33c31a3bdf3f9a4f53c5216d1333cd88
nxszy <nizinski.szy@gmail.com>
2024-03-10 21:51:23 +0100

    Added necessary changes to the conjverb model. Uploaded all 193 verbs to the database.

--- server/conjugation/models.py
+++ server/conjugation/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class ConjVerb(models.Model):
     
-    translation = models.CharField(max_length=30, primary_key=True)
+    translation = models.CharField(max_length=30)
     infinitive = models.CharField(max_length=30)
     sg_Ip_form = models.CharField(max_length=30)
     sg_IIp_form = models.CharField(max_length=30)
@@ -11,6 +11,9 @@ class ConjVerb(models.Model):
     pl_Ip_form = models.CharField(max_length=30)
     pl_IIp_form = models.CharField(max_length=30)
     pl_IIIp_form = models.CharField(max_length=30)
+
+    class Meta:
+        unique_together = (('translation', 'infinitive'),)
     
     def __str__(self):
         return self.translation
